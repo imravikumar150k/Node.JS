29448aff69e197807205fd3099fc55b6ae5a043d
Ravi Yadav <ravikumar150k@gmail.com>
2019-12-26 12:30:14 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const validator = require('validator');
 
 var a = validator.isEmail('kfhgj@nk.dkgn');
 
-console.log(a);
+console.log(5*19);
+
+const chalk = require('chalk');
+
+console.log(chalk.red('There  is a serious problem..'));
+
+console.log(chalk.bold.italic.red('Problem resolved..'));
 
 
 
